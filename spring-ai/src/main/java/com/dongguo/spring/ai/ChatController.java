f46dc8d795062ff432ac1fb73a3657c387bd2ea3
dongguo4812 <291320608@qq.com>
2025-04-03 06:59:58 +0800

    init

---
+++ spring-ai/src/main/java/com/dongguo/spring/ai/ChatController.java
@@ -0,0 +1,32 @@
+package com.dongguo.spring.ai;
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 阿里云百炼  通义千问
+ */
+@RestController
+public class ChatController {
+
+
+    private final DashScopeChatModel dashScopeChatModel;
+
+    public ChatController(DashScopeChatModel dashScopeChatModel) {
+        this.dashScopeChatModel = dashScopeChatModel;
+    }
+
+    @GetMapping("/chat/{msg}")
+    public String chat(@PathVariable("msg") String msg) {
+        /**
+         * this.chatclient.prompt():获取一个对话请求构建器，用于构建对话请求。
+         * .user(input)：设置用户的输入内容，input参数即为用户提供的文本。
+         * .call（):发送构建好的对话请求到服务端，并等待响应。
+         * .content（):从响应中提取并返回实际的内容，通常是服务端生成的回复文本。
+         */
+        return dashScopeChatModel.call(msg);
+    }
+}
+
