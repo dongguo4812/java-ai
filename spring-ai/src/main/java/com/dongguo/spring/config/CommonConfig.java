da5678a93fd582bd6d733fc2915bd65290a4f4a4
dongguo4812 <291320608@qq.com>
2025-04-03 15:36:13 +0800

    本地向量数据库实现RAG

--- spring-ai/src/main/java/com/dongguo/spring/config/CommonConfig.java
+++ spring-ai/src/main/java/com/dongguo/spring/config/CommonConfig.java
@@ -3,15 +3,28 @@ package com.dongguo.spring.config;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
+
+
+
     /**
      * 阿里百炼
      *
@@ -19,7 +32,7 @@ public class CommonConfig {
      * @return
      */
     @Bean
-    public ChatClient dashScopeChatClient(DashScopeChatModel dashScopeChatModel) {
+    public ChatClient dashScopeChatClient(DashScopeChatModel dashScopeChatModel,VectorStore vectorStore) {
         return ChatClient
                 .builder(dashScopeChatModel)
                 .defaultSystem("你是东郭商城的客户聊天支持代理，名字叫东郭智能客户，请以友好、乐于助人且愉快的方式来回复，" +
@@ -29,7 +42,9 @@ public class CommonConfig {
                         "在退订之前，请先获取订单信息并且需要用户确认信息，用户回答确认后才进行退订操作。")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),    //会话日志
-                        new MessageChatMemoryAdvisor(chatMemory()))   //会话存储
+                        new MessageChatMemoryAdvisor(chatMemory()), //会话存储
+                        new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder().build())  //RAG
+                )
                 .defaultFunctions("cancelOrder","getOrderInfo")  //默认的FunctionCall
                 .build();
     }
@@ -59,5 +74,36 @@ public class CommonConfig {
                         new SimpleLoggerAdvisor(),    //会话日志
                         new MessageChatMemoryAdvisor(chatMemory()))   //会话存储
                 .build();
+    }
+
+    /**
+     * 向量数据库使用内存实现
+     * @param embeddingModel
+     * @return
+     */
+    @Bean
+    public VectorStore vectorStore(
+            @Qualifier("dashscopeEmbeddingModel") EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel)
+                .build();
+    }
+
+    /**
+     * 读取资料转化并写入向量数据库
+     * @param embeddingModel
+     * @param vectorStore
+     * @param termsOfServiceDocs
+     * @return
+     */
+    @Bean
+    CommandLineRunner ingestTermOfServiceToVectorStore (
+            @Qualifier("dashscopeEmbeddingModel") EmbeddingModel embeddingModel,
+            VectorStore vectorStore, @Value("classpath:rag/terms-of-service.txt") String termsOfServiceDocs) {
+        return args -> {
+            vectorStore.write(  //3写入
+                    new TokenTextSplitter().transform(   //2切分转化
+                            new TextReader(termsOfServiceDocs).read()) //1读取
+            );
+        };
     }
 }
